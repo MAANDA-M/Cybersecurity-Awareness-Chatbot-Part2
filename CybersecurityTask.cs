c24540d7fc87fa477edc4d74a090a707f2edb884
MAANDA-M <163289914+MAANDA-M@users.noreply.github.com>
2026-05-29 20:07:19 +0200

    Upload project source code

---
+++ CybersecurityTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CybersecurityChatbotWPF
+{
+    public class CybersecurityTask
+    {
+        public string Title { get; private set; }
+        public DateTime? ReminderDate { get; private set; }
+        public string ReminderDescription { get; private set; }
+        public bool IsCompleted { get; private set; }
+
+        public CybersecurityTask(string title, DateTime? reminderDate = null, string reminderDescription = null)
+        {
+            Title = title;
+            ReminderDate = reminderDate;
+            ReminderDescription = reminderDescription;
+            IsCompleted = false;
+        }
+
+        public void MarkComplete()
+        {
+            IsCompleted = true;
+        }
+    }
+}
